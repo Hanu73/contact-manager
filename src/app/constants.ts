84286eaf3f14294bb57b50ff95e41e35e788f28e
Hanu <Hanu73@users.noreply.github.com>
2023-02-15 21:45:31 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -2,9 +2,9 @@ export function getBaseHeaders(): Record<string, string> {
   return { 'Content-type': 'application/json' };
 }
 
-export const VALID_EMAIL_DOMAINS = ['inmar'];
+export const VALID_EMAIL_DOMAINS = ['gmail'];
 export const EMAIL_REGEX =
-  '^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+.)?[a-zA-Z]+.)?(inmar).com$';
+  '^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+.)?[a-zA-Z]+.)?(gmail).com$';
 
 export const SUCCESS_ALERT_CLASS = 'alert-success';
 export const ERROR_ALERT_CLASS = 'alert-danger';
